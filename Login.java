e91b740863e039f1bde2f019455249d9cdae1da8
Amar Jena <jenaamar5@gmail.com>
2018-03-01 10:26:38 -0800

    Skipped stage for Login.java

--- Login.java
+++ Login.java
@@ -2,6 +2,7 @@ public class Login{
   public static void main (String []args){
     System.out.println("First line for Login func");     
     public void clear(){
+	System.out.println(sjjsjsjsj"");
 }
  }
 }
