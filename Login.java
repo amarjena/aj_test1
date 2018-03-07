c8e9340a9c7e826b7af9e725aafa13e15acf906d
Amar Jena <jenaamar5@gmail.com>
2018-03-07 07:40:08 -0800

    No change just line break done to test conflict on branch

--- Login.java
+++ Login.java
@@ -1,7 +1,14 @@
 public class Login{
   public static void main (String []args){
     System.out.println("First line for Login func");     
-    public void clear(){
+    
+
+
+
+
+
+
+  public void clear(){
 	System.out.println(sjjsjsjsj"");
 }
  }
